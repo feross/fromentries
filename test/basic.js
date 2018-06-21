ade8eb692098575e7b030109e058ada301727d2e
Feross Aboukhadijeh <feross@feross.org>
2018-06-21 12:16:07 -0700

    add tests

---
+++ test/basic.js
@@ -0,0 +1,23 @@
+const fromEntries = require('../')
+const test = require('tape')
+const { URLSearchParams } = require('url')
+
+test('works on an iterable collection', t => {
+  const map = new Map([ [ 'a', 1 ], [ 'b', 2 ], [ 'c', 3 ] ])
+  const obj = fromEntries(map)
+  t.deepEqual(obj, { a: 1, b: 2, c: 3 })
+  t.end()
+})
+
+test('works when entries() is explicitly called', t => {
+  const map = new Map([ [ 'a', 1 ], [ 'b', 2 ], [ 'c', 3 ] ])
+  const obj = fromEntries(map.entries())
+  t.deepEqual(obj, { a: 1, b: 2, c: 3 })
+  t.end()
+})
+
+test('works on map-like objects like URLSearchParams', t => {
+  const obj = fromEntries(new URLSearchParams('foo=bar&baz=qux'))
+  t.deepEqual(obj, { foo: 'bar', 'baz': 'qux' })
+  t.end()
+})
