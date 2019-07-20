48b50fb8bb417ba6fe0b4df3243587287212608d
Feross Aboukhadijeh <feross@feross.org>
2019-07-20 11:10:00 -0700

    standard

--- test/basic.js
+++ test/basic.js
@@ -3,14 +3,14 @@ const test = require('tape')
 const { URLSearchParams } = require('url')
 
 test('works on an iterable collection', t => {
-  const map = new Map([ [ 'a', 1 ], [ 'b', 2 ], [ 'c', 3 ] ])
+  const map = new Map([['a', 1], ['b', 2], ['c', 3]])
   const obj = fromEntries(map)
   t.deepEqual(obj, { a: 1, b: 2, c: 3 })
   t.end()
 })
 
 test('works when entries() is explicitly called', t => {
-  const map = new Map([ [ 'a', 1 ], [ 'b', 2 ], [ 'c', 3 ] ])
+  const map = new Map([['a', 1], ['b', 2], ['c', 3]])
   const obj = fromEntries(map.entries())
   t.deepEqual(obj, { a: 1, b: 2, c: 3 })
   t.end()
@@ -18,6 +18,6 @@ test('works when entries() is explicitly called', t => {
 
 test('works on map-like objects like URLSearchParams', t => {
   const obj = fromEntries(new URLSearchParams('foo=bar&baz=qux'))
-  t.deepEqual(obj, { foo: 'bar', 'baz': 'qux' })
+  t.deepEqual(obj, { foo: 'bar', baz: 'qux' })
   t.end()
 })
