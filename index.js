5d2cedaa77f1c1b985b5c78e84a638073366dde1
Feross Aboukhadijeh <feross@feross.org>
2020-07-16 19:48:25 -0700

    package metadata

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*! fromentries. MIT License. Feross Aboukhadijeh <https://feross.org/opensource> */
 module.exports = function fromEntries (iterable) {
   return [...iterable].reduce((obj, [key, val]) => {
     obj[key] = val
